c61605210da2a1ea6997fa301f7ad336a062919f
Nonato <nonato@amazing.codes>
2023-06-22 19:38:37 -0300

    Stage 4

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   def apply_discount_coupon
     order = Order.find(params[:id])
     coupon = Coupon.find_by!(code: params[:coupon_code])
+    set_discount_calculator_for(coupon)
     order.apply_discount_coupon!(coupon)
 
     render json: order
@@ -10,4 +11,17 @@ class OrdersController < ApplicationController
     render json: { error: error.to_s }
   end
 
+  private
+
+  def set_discount_calculator_for(coupon)
+    if coupon.created_at.to_date.to_s == '2022-11-25'
+      calculator = CouponDiscountCalculators::BlackFriday2022.new
+    elsif coupon.percentage?
+      calculator = CouponDiscountCalculators::Percentage.new
+    elsif coupon.fixed?
+      calculator = CouponDiscountCalculators::Fixed.new
+    end
+
+    coupon.discount_calculator = calculator
+  end
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,22 +1,12 @@
 class Coupon < ApplicationRecord
   enum discount_type: [:percentage, :fixed]
+  
+  attr_accessor :discount_calculator
 
   def calculate_discount_for(amount)
     discount = discount_calculator.calculate(amount, self)
     is_applicable = discount > 0
 
     return is_applicable, discount
-  end
-
-  private
-
-  def discount_calculator
-    if self.created_at.to_date.to_s == '2022-11-25'
-      CouponDiscountCalculators::BlackFriday2022.new
-    elsif self.percentage?
-      CouponDiscountCalculators::Percentage.new
-    elsif self.fixed?
-      CouponDiscountCalculators::Fixed.new
-    end
   end
 end
