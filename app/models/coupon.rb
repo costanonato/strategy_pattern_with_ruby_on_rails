70e3e8854c9ad9ad9346cd853a083c8accab761d
Nonato <nonato@amazing.codes>
2023-06-22 09:39:55 -0300

    Stage 1

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,16 +1,30 @@
 class Coupon < ApplicationRecord
-  enum discount_type: [:percentage]
+  enum discount_type: [:percentage, :fixed]
 
   # Calculates discount for a given amount.
-  # Full discount is applied for amounts >= 1000
-  # Half discount is applied for amounts >= 700
-  # No discount is applied for amounts < 700
   def calculate_discount_for(amount)
-    full_discount = (amount * self.discount_value) / 100
+    # [Percentage]
+    # Full discount is applied for amounts >= 1000
+    # Half discount is applied for amounts >= 700
+    # No discount is applied for amounts < 700
+    if self.percentage?
+      full_discount = (amount * self.discount_value) / 100
 
-    return true, full_discount   if amount >= 1000
-    return true, full_discount/2 if amount >= 700
-    return false, 0
+      return true, full_discount   if amount >= 1000
+      return true, full_discount/2 if amount >= 700
+      return false, 0
+
+    # [Fixed]
+    # Full discount is applied for amounts >= 2000
+    # 1/4 discount is applied for amounts >= 1500
+    # No discount is applied for amounts < 1500
+    elsif self.fixed?
+      full_discount = self.discount_value
+
+      return true, full_discount   if amount >= 2000
+      return true, full_discount/4 if amount >= 1500
+      return false, 0
+    end
   end
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,26 @@ Coupon.create!(
   discount_value: 10,
   code: '10PercentOff'
 )
+
+########
+
+Order.create!(
+  subtotal: 2300,
+  total: 2300
+)
+
+Order.create!(
+  subtotal: 1700,
+  total: 1700
+)
+
+Order.create!(
+  subtotal: 1200,
+  total: 1200
+)
+
+Coupon.create!(
+  discount_type: :fixed,
+  discount_value: 400,
+  code: '400ContoOff'
+)
